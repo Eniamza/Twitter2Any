2da9c5b4486cb6e76bd4c5d0beea6d753618c985
Eniamza <eniamza0@gmail.com>
2023-09-07 01:42:39 +0600

    Feat: Complete Tweet Feed

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const fs = require('fs'); //FS Requiring
 const recordfile = "./record.json"
 const record = require(recordfile);
 
+let send_msg = {
+	username:"Dooggies",
+	"content": ""
+  }
+
 const {fetchlastTweet} = require("./modules/functions")
 
 const {lastTweetTime,LastTweetID} = require("./record.json") //Importing file to keep track of the last Tweet
@@ -14,6 +19,14 @@ let lastTime = lastTweetTime
 let check_duration = 3600000 // Interval For Schecking
 
 const { Rettiwt } = require('rettiwt-api');
+const { default: fetch } = require('node-fetch');
+const { default: axios } = require('axios');
+const { send } = require('process');
+
+function send_web(send_msg){
+	axios.post(process.env.WEBHOOK, send_msg)
+	  
+}
 
 const rettiwt = new Rettiwt(process.env.COOKIE);
 
@@ -41,6 +54,9 @@ function checkNew() {
 		record.LastTweetID = newTweet.id
 		console.log(`New Tweet Detected! Link: https://twitter.com/eniamza/status/${newTweet.id}`)
 		fs.writeFileSync(recordfile, JSON.stringify(record,null,2));
+		send_msg.content = `https://twitter.com/eniamza/status/${newTweet.id}`
+		console.log(send_msg)
+		send_web(send_msg)
 	
 	})
 	.catch(err => {
