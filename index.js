8ff74f2ef125886b0a9954ffeceb6965f8ed2e93
Eniamza <eniamza0@gmail.com>
2023-09-06 22:29:51 +0600

    Feat: Implement Tweet Filter Logic and Interval

--- index.js
+++ index.js
@@ -8,18 +8,42 @@ const {fetchlastTweet} = require("./modules/functions")
 
 const {lastTweetTime,LastTweetID} = require("./record.json") //Importing file to keep track of the last Tweet
 
+let LastID = LastTweetID
+let lastTime = lastTweetTime
+
 let check_duration = 3600000 // Interval For Schecking
 
 const { Rettiwt } = require('rettiwt-api');
 
 const rettiwt = new Rettiwt(process.env.COOKIE);
 
-rettiwt.tweet.search({
-	fromUsers: ['DooggiesNFT']
-})
-.then(data => {
-	fetchlastTweet(data)
-})
-.catch(err => {
-	console.log(err)
-});
+let newTweet
+
+setInterval(checkNew, 5000);
+
+function checkNew() {
+	rettiwt.tweet.search({
+		fromUsers: ['Eniamza']
+	})
+	.then(data => {
+		
+		newTweet = fetchlastTweet(data,LastID,lastTime)
+		
+		if(newTweet == undefined){
+			let d = new Date()
+			console.log(`No new Tweets at ${d.toGMTString()}`)
+			return
+		}
+	
+		lastTime = Date.parse(newTweet.createdAt)
+		LastID = record.LastTweetID = newTweet.id
+		record.lastTweetTime = Date.parse(newTweet.createdAt)
+		record.LastTweetID = newTweet.id
+		console.log(`New Tweet Detected! Link: https://twitter.com/eniamza/status/${newTweet.id}`)
+		fs.writeFileSync(recordfile, JSON.stringify(record,null,2));
+	
+	})
+	.catch(err => {
+		console.log(err)
+	})
+}
--- modules/functions.js
+++ modules/functions.js
@@ -1,7 +1,20 @@
-const fetchlastTweet = function (usrData) {
+const fetchlastTweet = function (usrData,lastID,lastTime) {
 
+    let tweetlist = usrData.list
+    let tweetlen = tweetlist.length
     
-    console.log(usrData.list[0])
+
+    for(let i = 0; i<tweetlen;i++)
+    {
+
+        let currTweet = tweetlist[i]
+        if(currTweet.id != lastID && Date.parse(currTweet.createdAt)>lastTime && currTweet.quoted == undefined && currTweet.replyTo == undefined)
+        {
+            
+            return currTweet
+        }
+
+    }
 
 }
 
